7ce21294954cb9497174176953633fe7df23b4ef
patel.nimai@gmail.com <patel.nimai@gmail.com>
2024-10-09 15:05:52 -0400

    Fixed rideRoutes.js

--- routes/rideRoutes.js
+++ routes/rideRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const rideSenderController = require('../controllers/rideSender');
+const priceCalculatorController = require('../controllers/priceCalculator');  // Correct import for price calculation
 
-router.post('/sendRide', rideSenderController.sendRide);
+router.get('/calculatePrice', priceCalculatorController.calculatePrice);  // Route for calculating price
 
 module.exports = router;
