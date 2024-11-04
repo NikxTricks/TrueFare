e0549bd04cc751ac123e313c354f85583a1efb7f
RohitGeorge1 <rohitkgeorge@gmail.com>
2024-11-04 16:12:17 -0500

    config changes

--- config/config.js
+++ config/config.js
@@ -1,50 +1,17 @@
-// config/config.js
-require('dotenv').config();
-
 module.exports = {
-  "development": {
-    "username": process.env.DB_USER || "your_db_username",
-    "password": process.env.DB_PASS || "your_db_password",
-    "database": process.env.DB_NAME || "your_database_name",
-    "host": process.env.DB_HOST || "localhost",
-    "port": process.env.DB_PORT || 5432,
-    "dialect": "postgres",
-    "dialectOptions": {
-      "ssl": process.env.DB_SSL === 'true' ? {
-        "require": true,
-        "rejectUnauthorized": false // Adjust as per your SSL requirements
-      } : false
-    },
-    "logging": false
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
   },
-  "test": {
-    "username": process.env.DB_USER || "your_db_username",
-    "password": process.env.DB_PASS || "your_db_password",
-    "database": process.env.DB_NAME || "your_database_name",
-    "host": process.env.DB_HOST || "localhost",
-    "port": process.env.DB_PORT || 5432,
-    "dialect": "postgres",
-    "dialectOptions": {
-      "ssl": process.env.DB_SSL === 'true' ? {
-        "require": true,
-        "rejectUnauthorized": false
-      } : false
-    },
-    "logging": false
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    use_env_variable: 'DATABASE_URL',
   },
-  "production": {
-    "username": process.env.DB_USER || "your_db_username",
-    "password": process.env.DB_PASS || "your_db_password",
-    "database": process.env.DB_NAME || "your_database_name",
-    "host": process.env.DB_HOST || "localhost",
-    "port": process.env.DB_PORT || 5432,
-    "dialect": "postgres",
-    "dialectOptions": {
-      "ssl": process.env.DB_SSL === 'true' ? {
-        "require": true,
-        "rejectUnauthorized": false
-      } : false
-    },
-    "logging": false
-  }
 };
